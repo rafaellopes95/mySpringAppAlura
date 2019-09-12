cc78bb36f36849dbf3548047d9c6af0fcd35e3a6
Rafael Lopes <rafa_lopes_leme@hotmail.com>
2019-09-11 22:09:31 -0300

    Class 12: Application, Request and Session scopes
    In this class, the usage of these 3 different scopes was applied in the app.

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
@@ -1,9 +1,11 @@
 package br.com.casadocodigo.loja.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
@@ -13,12 +15,21 @@ import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
 
 /**
- *  Controller para manipular o carrinho de compras.
- *
+ * Controller para manipular requisições do carrinho de compras.
+ * Por default o escopo é de application, o que significa que este controller seria único para
+ * todos os usuários (sigleton). É mais comum que controllers tenham escopo de request, cuja
+ * intância dura apenas o tempo da requisição (um objeto do controller é criado para cada
+ * request, e "morre" após seu término).
+ * 
+ * Controller no geral tem um papel bem definido, ele simplesmente trata a requisição. Ele recebe 
+ * os dados, repassa para um outro objeto e devolve uma resposta. Pensando assim, podemos concluir 
+ * que após a requisição ser atendida, não faz sentido que o controller ainda esteja "vivo". 
+ * Geralmente, o escopo dos controllers é o de request. Isto significa que quando há uma requisição, 
+ * o controller é criado e depois, ela deixa de existir.
  */
-
 @Controller
 @RequestMapping("/carrinho")
+@Scope(value = WebApplicationContext.SCOPE_REQUEST)
 public class CarrinhoComprasController {
 
 	@Autowired
--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -3,9 +3,18 @@ package br.com.casadocodigo.loja.models;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
+/**
+ * Bean para gerenciar o carrinho de compras na loja.
+ * Por default, o escopo é de application, o que significa que este component seria único para
+ * todos os usuários (sigleton). Isso não faz sentido, já que deveria existir um carrinho de compras
+ * pra cada usuário, por isso o escopo de sessão (um objeto desse é criado para cada sessão de usuário).
+ */
 @Component
+@Scope(value = WebApplicationContext.SCOPE_SESSION)
 public class CarrinhoCompras {
 	
 	private Map<CarrinhoItem, Integer> itens = new LinkedHashMap<CarrinhoItem, Integer>();
